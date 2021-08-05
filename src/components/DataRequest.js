cac70442e514d06bc64be103511df5e06ce7f53d
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-08-05 13:16:49 -0500

    build handleSubmit and handleChanhe function and set my state using useState

--- src/components/DataRequest.js
+++ src/components/DataRequest.js
@@ -2,10 +2,11 @@ import React from 'react'
 import axios from 'axios'
 
 const DataRequest = async (query) => {
-    const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+     const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`
+    // const url = `https://api.edamam.com/search?q=${query}&app_id=851a0457&app_key=a4eaadda6db0afd1112d6f41d68fbd3f`
     if (query !== "") {
         const result = await axios.get(url)
-        console.log(url)
+        console.log(result)
     }
     
 }
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
-
+import DataRequest from './DataRequest'
 function SearchInput() {
     const [query, setQuery] = useState("")
+
+   const handleChange = e => {
+       setQuery(e.target.value)
+    }
+
+   const handleSubmit = e => {
+        e.preventDefault()
+        DataRequest(query)
+
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text"
                 name="query"
                 value={query} 
                 autoComplete="off"
-                placeholder="Search Food" />
+                placeholder="Search Food" 
+                onChange={handleChange}/>
                 <input type='submit' value="Search" />
             </form>
             
