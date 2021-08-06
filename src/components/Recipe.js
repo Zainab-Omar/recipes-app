a897daead565066d0285f52724ffe05cb2ae48d7
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-08-05 19:29:41 -0500

    add useState to Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,18 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import Recipes from './Recipes'
+import RecipeDetails from './RecipeDetails'
 function Recipe({recipe}) {
     const {label, image, url, calories, cautions, ingredients} = recipe.recipe
+    const [toggle, setToggle] = useState(false)
     return (
         <div className="recipe">
            <img src={image} alt="food-picture" />
            <h4>{label}</h4>
            <h4>calories: {calories}</h4>
            <h4>cautions: {cautions.map(caution => <li>{caution}</li>)}</h4>
-           <a href={url} target="_blank">Recipe URL</a>
-
+           <a href={url} target="_blank">Recipe URL</a><br />
+           <button onClick={() => setToggle(!toggle)}>Ingredients</button>
+            {toggle && <RecipeDetails ingredients={ingredients} />}
         </div>
     )
 }
