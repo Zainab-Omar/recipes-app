da50afb88ef191ef1de74b4789563a934c337701
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-08-05 19:53:48 -0500

    add uuid to RecipeDetails

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 function RecipeDetails({ingredients}) {
    return ingredients.map(ingredient => {
     return (
-        <ul>
+        <ul key={uuidv4()}>
             <li>{ingredient.text}</li>
         </ul>
             
