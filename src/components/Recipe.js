50893063c7604802fecfe0b6d554cc4532a18d1a
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-08-05 19:24:34 -0500

    display recipes in recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Recipe({recipe}) {
-    const {label, image, url, ingredients} = recipe.recipe
+    const {label, image, url, calories, cautions, ingredients} = recipe.recipe
     return (
-        <div>
-           {console.log(recipe)}
+        <div className="recipe">
+           <img src={image} alt="food-picture" />
+           <h4>{label}</h4>
+           <h4>calories: {calories}</h4>
+           <h4>cautions: {cautions.map(caution => <li>{caution}</li>)}</h4>
+           <a href={url} target="_blank">Recipe URL</a>
+
         </div>
     )
 }
