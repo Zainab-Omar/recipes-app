afed90022e043a3565d4f06d055c4d4044ada10f
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-08-13 14:38:53 -0500

    add title to my app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchInput from "./components/SearchInput";
 function App() {
   return (
     <div className="App">
+      <h1> Welcome to Recipe App </h1>
      <SearchInput />
     </div>
   );
